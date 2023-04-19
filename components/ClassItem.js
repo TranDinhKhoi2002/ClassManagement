733919ade61ea1353fe7390d0b07ccc8aedb1172
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2023-04-19 20:22:10 +0700

    handle get students quantity

--- components/ClassItem.js
+++ components/ClassItem.js
@@ -1,11 +1,29 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {Text, StyleSheet, Platform, Pressable, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, StyleSheet, Platform, Pressable, View, Alert} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {screenNames} from '../config/screens';
+import {getDBConnection, getStudents} from '../db/db-services';
 
 function ClassItem({item}) {
   const navigation = useNavigation();
+  const [studentsQuantity, setStudentsQuantity] = useState();
+
+  useEffect(() => {
+    const getStudentsInClass = async () => {
+      try {
+        const db = await getDBConnection();
+        const students = await getStudents(db, item.id);
+        setStudentsQuantity(students.length);
+      } catch (error) {
+        Alert.alert('Something went wrong', 'Failed to load students', [
+          {text: 'Cancel', style: 'cancel'},
+        ]);
+      }
+    };
+
+    getStudentsInClass();
+  }, [item.id]);
 
   const handlePress = () => {
     navigation.navigate(screenNames.classDetails.name, {classItem: item});
@@ -19,7 +37,7 @@ function ClassItem({item}) {
         onPress={handlePress}>
         <Text style={styles.classDetail}>Id: {item.id}</Text>
         <Text style={styles.classDetail}>Name: {item.name}</Text>
-        <Text style={styles.classDetail}>Students: 40</Text>
+        <Text style={styles.classDetail}>Students: {studentsQuantity}</Text>
         <Text style={styles.order}>#1</Text>
       </Pressable>
     </View>
